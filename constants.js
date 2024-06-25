f0b39c1b45f1fee5a33d3a9728e7100b04272a15
Shubpreet Kour <preetshub36@gmail.com>
2024-06-25 20:35:54 +0530

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,274 @@
+import Sign_Up from '../images/Sign_Up.png'
+import skycast from '../images/Skycast.png'
+import movie from '../images/movie.png'
+import shopper from '../images/Shopper.png'
+
+export const Bio = {
+  name: "Shub Preet Kour",
+  roles: [
+    "Web Developer",
+    "Data Scientist",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning, I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/shubpreetkour",
+  resume:
+    "https://drive.google.com/file/d/1BNZlqzolTWIibyJKF09Qo4V4qig53ksh/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/shubpreet-kour-547071202/",
+  insta: "https://www.instagram.com/shub.mehta922/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "XML",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+    role: "DevOps & Fullstack Engineering Intern",
+    company: "Flipr Innovations Pvt. Ltd.",
+    date: "Aug 2023 - July 2023",
+    desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+    skills: [
+      "Docker",
+      "Terraform",
+      "AWS",
+      "EC2",
+      "Portainer",
+      "Nginx",
+      "JavaScript",
+      "TypeScript",
+      "Node.js",
+      "Next.js",
+    ],
+    doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+  {
+    id: 2,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/neurobit.jpeg?alt=media&token=1cacbb7b-e5ea-4efb-9b3d-f5a31b9564ad",
+    role: "Frontend Engineer Intern",
+    company: "Neurobit",
+    date: "June 2023 - Present",
+    desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "Node.js",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+  },
+  {
+    id: 3,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+    role: "Fullstack Externship",
+    company: "Flipr",
+    date: "June 2023 - July 2023",
+    desc: "Built an employee management full stack web app, used Docker and deployed on AWS EC2. I was the top performer in the program.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "Node.js",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Docker",
+      "AWS",
+      "MongoDB",
+    ],
+  },
+  {
+    id: 4,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+    role: "Open Source Contributor",
+    company: "GirlScript Summer of Code",
+    date: "May 2023 - Present",
+    desc: "Contributing to different open-source projects and learning from industry experts.",
+    doc: "https://media.licdn.com/dms/image/D4D22AQGzSpgFaR4hsQ/feedshare-shrink_800/0/1684079632872?e=1687996800&v=beta&t=i0xWn_k32Oj9SxKfc6rARSbOghXD9Ge6WyEb2p2vvrc",
+  },
+  {
+    id: 5,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/gdsc.jpeg?alt=media&token=c162329c-efaa-4be8-a173-8d3f4c48ea70",
+    role: "Data Science",
+    company: "DSC KIIT",
+    date: "Nov 2021 - Present",
+    desc: "As a data science student at the IIT Madras, I have had the opportunity to work on exciting projects and collaborate with talented developers who share my passion for technology. Through my involvement with GDCS, I have also had the opportunity to host and participate in numerous events, including hackathons, study jams, and workshops.",
+    skills: [
+      "Leadership",
+      "Statistics",
+      "Math",
+      "Python",
+      "Figma",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://cracku.in/latest-govt-jobs/wp-content/uploads/2019/10/GNDU-Logo.jpg",
+    school: "Guru Nanak Dev University, Amritsar",
+    date: "2021 - 2025",
+    grade: "7.40 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Electronics and Computer Engineering at Guru Nanak Dev University, Amritsar. I have completed 5 semesters and have a CGPA of 7.40. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Technology - BTech, Electronics and Computer Engineering",
+  },
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-BVjk1CBl38SYGw4zxjpqU-cbXmfK1-t73Q&s",
+    school: "India Institude of Technology Madrass",
+    date: "2021 - present",
+    grade: "6 CGPA",
+    desc: "I am currently pursuing a BSC from IIT madrass in Data Sciene and Programming",
+    degree: "Bachelor of Science- Data Science and Programming",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSuio8wxvVMfpbRKuXhHBTmEtiJr8jznv8xQ&s",
+    school: "Presentation Convent Higher Secondary School, Srinagar",
+    date: "2020 - 2021",
+    grade: "90.5%",
+    desc: "I completed my class 12 high school education at Presentation Convent Higher Secondary School.",
+    degree: "CBSE(XII)",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSuio8wxvVMfpbRKuXhHBTmEtiJr8jznv8xQ&s",
+    school: "Presentation Convent Higher Secondary School, Srinagar",
+    date: "2018 - 2019",
+    grade: "98%",
+    desc: "I completed my class 10 education at Presentation Convent Higher Secondary School.",
+    degree: "CBSE(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "REAL ESTATE",
+    date: "2024",
+    description:
+      "REAL ESTATE is a modern and fully responsive platform for buying and selling properties, built using the MERN stack (MongoDB, Express, React, Node.js). This platform provides a seamless experience with features like user authentication, adding properties and video chat.",
+    image: shopper,
+    tags: [
+      "React Js",
+      "MongoDB",
+      "Node Js",
+      "Express Js",
+    ],
+    category: "web app",
+    github: "https://github.com/shubpreetkour/Real-Estate-Project",
+  },
+  {
+    id: 1,
+    title: "Crowdfunding dapp",
+    date: "2024",
+    description:
+      "Crowdfunding decentralized applications (DApps) represent a significant innovation in the realm of fundraising, leveraging blockchain technology to create transparent, efficient, and secure platforms for raising capital.",
+    image: movie,
+    tags: ["Solidity", "CSS", "JavaScript"],
+    category: "web app",
+    github: "https://github.com/shubpreetkour/CrowdFunding",
+  },  
+  {
+    id: 2,
+    title: "Todo Dapp",
+    date: "2023",
+    description:
+      "To-Do List decentralized applications (DApps) are an innovative solution for task management, leveraging blockchain technology to provide secure, decentralized, and transparent platforms for organizing and tracking tasks.",
+    image: Sign_Up,
+    tags: ["Solidity", "CSS", "HTML"],
+    category: "web app",
+    github: "https://github.com/shubpreetkour/Todo-dapp",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
